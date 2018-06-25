85161e6a57bb336abe0096a6102e41d04a7dcdc6
kayisheng <3024768596@qq.com>
2018-06-25 16:55:31 +0800

    [feature]完善细节

--- app/src/main/java/com/yunkahui/datacubeper/mine/adapter/MyCardAdapter.java
+++ app/src/main/java/com/yunkahui/datacubeper/mine/adapter/MyCardAdapter.java
@@ -7,31 +7,35 @@ import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.BaseViewHolder;
 import com.yunkahui.datacubeper.R;
 import com.yunkahui.datacubeper.base.CardDoctorApplication;
+import com.yunkahui.datacubeper.common.bean.BillCreditCard;
 import com.yunkahui.datacubeper.common.bean.MineItem;
 import com.yunkahui.datacubeper.common.bean.MyCardBean;
+import com.yunkahui.datacubeper.common.utils.DataUtils;
+import com.yunkahui.datacubeper.common.utils.TimeUtils;
 
 import java.util.List;
 
 /**
  * Created by YD1 on 2018/4/3
  */
-public class MyCardAdapter extends BaseQuickAdapter<MyCardBean, BaseViewHolder> {
+public class MyCardAdapter extends BaseQuickAdapter<BillCreditCard.CreditCard, BaseViewHolder> {
 
     public MyCardAdapter(int layoutResId, List data) {
         super(layoutResId, data);
     }
 
     @Override
-    protected void convert(BaseViewHolder helper, MyCardBean item) {
+    protected void convert(BaseViewHolder helper, BillCreditCard.CreditCard item) {
         if (item != null) {
             helper.setVisible(R.id.iv_sample, false);
             Resources resources = CardDoctorApplication.getContext().getResources();
-            helper.setBackgroundRes(R.id.iv_bank_icon, item.getIcon());
+            helper.setBackgroundRes(R.id.iv_bank_icon, DataUtils.getBankIconForName(item.getBankCardName()));
             helper.setText(R.id.tv_bank_name, item.getBankCardName());
-            helper.setText(R.id.tv_bank_card_id, String.format(resources.getString(R.string.format_card_id), item.getBankCardId()));
+            String num = item.getBankCardNum().substring(item.getBankCardNum().length() - 4, item.getBankCardNum().length());
+            helper.setText(R.id.tv_bank_card_id, String.format(resources.getString(R.string.format_card_id), num));
             helper.setText(R.id.tv_card_holder, item.getCardHolder());
-            helper.setText(R.id.tv_bill_date, String.format(resources.getString(R.string.format_bill_date), item.getBillDate()));
-            helper.setText(R.id.tv_repay_date, String.format(resources.getString(R.string.format_repay_date), item.getRepayDate()));
+            helper.setText(R.id.tv_bill_date, String.format(resources.getString(R.string.format_bill_date), TimeUtils.format("MM-dd", item.getBillDayDate())));
+            helper.setText(R.id.tv_repay_date, String.format(resources.getString(R.string.format_repay_date), TimeUtils.format("MM-dd", item.getRepayDayDate())));
 
             helper.addOnClickListener(R.id.tv_unbind);
         } else {
